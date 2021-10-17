ee47b4bac3093d57ca98d32ad2af65477949143c
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-10-17 20:32:29 +0300

    Auto plus

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const autoModelInput = document.getElementById('model');
 const autoDateInput = document.getElementById('date');
 const autoColorInput = document.getElementById('color');
-const autoFuelInput = document.getElementById('fuel');
-const addToListButton = document.getElementById('button');
+const autoFuelInput = document.getElementById("fuel");
+const addButton = document.getElementById('save');
+const carsList = document.getElementById('list');
+const addCarForm = document.getElementById('add_car');
+const updateCarForm = document.getElementById('update_car');
+const modelUpdateInput = document.getElementById("updateModel");
+const dateUpdateInput = document.getElementById("updateDate");
+const colorUpdateInput = document.getElementById("updateColor");
+const fuelUpdateInput = document.getElementById("updateFuel");
+let updateCar;
 var FuelType;
 (function (FuelType) {
-    FuelType[FuelType["Diesel"] = 0] = "Diesel";
-    FuelType[FuelType["Gasoline"] = 1] = "Gasoline";
+    FuelType[FuelType["Dyzelis"] = 0] = "Dyzelis";
+    FuelType[FuelType["Benzinas"] = 1] = "Benzinas";
 })(FuelType || (FuelType = {}));
 class Car {
     constructor(model, date, color, fuel) {
@@ -14,10 +22,93 @@ class Car {
         this.date = new Date(date);
         this.color = color;
         this.fuel = +fuel;
+        this.id = Math.round(Math.random() * 1000);
+    }
+    printCar(element) {
+        element.innerHTML += `<div class="entry">
+                <div class="entry_parameter">${this.model}</div>
+                <div class="entry_parameter">${this.date.toISOString().slice(0, 10)}</div>
+                <div class="entry_parameter">${this.color}</div>
+                <div class="entry_parameter">${FuelType[this.fuel]}</div>
+                <div class="actions">
+                    <img class="update" onclick='onUpdateCar(${this.id})' src='./img/update.png' alt="Atnaujinti">
+                    <img class="delete" onclick='onDeleteCar(${this.id})' src='./img/delete.png' alt="Istrinti">
+                </div>
+            </div>`;
     }
 }
-let cars = [];
-addToListButton === null || addToListButton === void 0 ? void 0 : addToListButton.addEventListener('click', () => {
+const autoPark = [];
+addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener('click', (e) => {
+    if (autoModelInput.value === '' ||
+        autoDateInput.value === '' ||
+        autoColorInput.value === '' ||
+        autoFuelInput.value === '') {
+        alert("Klaida, langelis negali buti tuscias...");
+        return;
+    }
+    ;
     const car = new Car(autoModelInput.value, autoDateInput.value, autoColorInput.value, autoFuelInput.value);
     console.log('Mygtukas ..');
+    autoPark.push(car);
+    publishCars();
+    autoModelInput.value = '';
+    autoDateInput.value = '';
+    autoColorInput.value = '';
+    autoFuelInput.value = '';
 });
+function publishCars(filter) {
+    carsList.innerHTML = '';
+    for (const car of autoPark) {
+        if (filter === undefined || filter === car.fuel) {
+            car.printCar(carsList);
+            console.log(car.model);
+        }
+    }
+}
+function onUpdateCar(id) {
+    for (const car of autoPark) {
+        if (id === car.id) {
+            updateCar = car;
+        }
+    }
+    console.log(updateCar);
+    populateUpdateForm();
+    addCarForm.classList.add("hide");
+    updateCarForm.classList.remove("hide");
+}
+function populateUpdateForm() {
+    modelUpdateInput.value = updateCar.model;
+    dateUpdateInput.value = updateCar.date.toISOString().slice(0, 10);
+    colorUpdateInput.value = updateCar.color;
+    fuelUpdateInput.value = updateCar.fuel.toString();
+}
+function onSave() {
+    updateCar.model = modelUpdateInput.value;
+    updateCar.date = new Date(dateUpdateInput.value);
+    updateCar.color = colorUpdateInput.value;
+    updateCar.fuel = +fuelUpdateInput.value;
+    addCarForm.classList.remove("hide");
+    updateCarForm.classList.add("hide");
+    publishCars();
+}
+function onDeleteCar(id) {
+    let deleteCar;
+    for (const car of autoPark) {
+        if (id === car.id) {
+            deleteCar = car;
+        }
+    }
+    if (deleteCar) {
+        autoPark.splice(autoPark.indexOf(deleteCar), 1);
+    }
+    publishCars();
+}
+function allCarsList() {
+    publishCars();
+}
+function dyzelCarsList() {
+    publishCars(FuelType.Dyzelis);
+}
+function gasCarsList() {
+    publishCars(FuelType.Benzinas);
+}
--- index.ts
+++ index.ts
@@ -1,21 +1,33 @@
-// HTMLInputElement nurodo kokio tipo elementa grazinam, gali buti null arba HTML imout
+// HTMLInputElement nurodo kokio tipo elementa grazinam, gali buti null arba HTML element
 const autoModelInput = document.getElementById('model') as HTMLInputElement;
 const autoDateInput = document.getElementById('date') as HTMLInputElement;
 const autoColorInput = document.getElementById('color') as HTMLInputElement;
-const autoFuelInput = document.getElementById('fuel') as HTMLSelectElement;
+const autoFuelInput = document.getElementById("fuel") as HTMLSelectElement;
+const addButton = document.getElementById('save');
+const carsList = document.getElementById('list') as HTMLInputElement;
 
-const addToListButton = document.getElementById('button');
+const addCarForm = document.getElementById('add_car') as HTMLElement;
+const updateCarForm = document.getElementById('update_car') as HTMLElement;
+
+const modelUpdateInput = document.getElementById("updateModel") as HTMLInputElement;
+const dateUpdateInput = document.getElementById("updateDate") as HTMLInputElement;
+const colorUpdateInput = document.getElementById("updateColor") as HTMLInputElement;
+const fuelUpdateInput = document.getElementById("updateFuel") as HTMLInputElement;
+
+
+let updateCar: Car; // atnaujinamo automobilio objektas
 
 enum FuelType {
-    Diesel = 0,
-    Gasoline = 1
+    Dyzelis,
+    Benzinas
 }
 
 class Car {
-    public readonly model: string;
-    public readonly date: Date;
-    public readonly color: string;
-    public readonly fuel: FuelType;
+    public model: string;
+    public date: Date;
+    public color: string;
+    public fuel: FuelType;
+    public id: number;
 
     constructor(
         model: string,
@@ -27,21 +39,119 @@ class Car {
         this.date = new Date(date);
         this.color = color;
         this.fuel = +fuel;
+        this.id = Math.round(Math.random() * 1000);
+    }
+
+    public printCar(element: HTMLElement) {
+        // element.innerHTML += `${this.model}`
+        element.innerHTML += `<div class="entry">
+                <div class="entry_parameter">${this.model}</div>
+                <div class="entry_parameter">${this.date.toISOString().slice(0, 10)}</div>
+                <div class="entry_parameter">${this.color}</div>
+                <div class="entry_parameter">${FuelType[this.fuel]}</div>
+                <div class="actions">
+                    <img class="update" onclick='onUpdateCar(${this.id})' src='./img/update.png' alt="Atnaujinti">
+                    <img class="delete" onclick='onDeleteCar(${this.id})' src='./img/delete.png' alt="Istrinti">
+                </div>
+            </div>`;
     }
 }
 
 // kokio tipo duomenis saugosim 
-let cars: Car[] = [];
+const autoPark: Car[] = [];
 
 // ? optional chaining jei reiksme null arba undefined nepridesime event listener
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
-addToListButton?.addEventListener('click', () => {
+addButton?.addEventListener('click', (e) => {
+    if (autoModelInput.value === '' ||
+        autoDateInput.value === '' ||
+        autoColorInput.value === '' ||
+        autoFuelInput.value === '') {
+        alert("Klaida, langelis negali buti tuscias...")
+        return
+    };
+
     const car = new Car(
         autoModelInput.value,
         autoDateInput.value,
         autoColorInput.value,
-        autoFuelInput.value)
+        autoFuelInput.value);
 
     console.log('Mygtukas ..');
+    autoPark.push(car);
 
+    publishCars();
+
+    autoModelInput.value = '';
+    autoDateInput.value = '';
+    autoColorInput.value = '';
+    autoFuelInput.value = '';
 });
+
+function publishCars(filter?: FuelType) {
+    carsList.innerHTML = '';
+    for (const car of autoPark) {
+        if (filter === undefined || filter === car.fuel) {
+            car.printCar(carsList);
+            console.log(car.model);
+        }
+    }
+}
+
+function onUpdateCar(id: number): void {
+    for (const car of autoPark) {
+        if (id === car.id) {
+            updateCar = car;
+        }
+    }
+    console.log(updateCar);
+    populateUpdateForm();
+
+    addCarForm.classList.add("hide");
+    updateCarForm.classList.remove("hide");
+}
+
+function populateUpdateForm() {
+    modelUpdateInput.value = updateCar.model;
+    dateUpdateInput.value = updateCar.date.toISOString().slice(0, 10);
+    colorUpdateInput.value = updateCar.color;
+    fuelUpdateInput.value = updateCar.fuel.toString();
+}
+
+function onSave() {
+    updateCar.model = modelUpdateInput.value;
+    updateCar.date = new Date(dateUpdateInput.value);
+    updateCar.color = colorUpdateInput.value;
+    updateCar.fuel = +fuelUpdateInput.value;
+
+    addCarForm.classList.remove("hide");
+    updateCarForm.classList.add("hide");
+
+    publishCars();
+}
+
+function onDeleteCar(id: number) {
+    let deleteCar: Car | undefined; //= autoParkas[0];
+    for (const car of autoPark) {
+        if (id === car.id) {
+            deleteCar = car;
+        }
+    }
+    if (deleteCar) {
+        autoPark.splice(autoPark.indexOf(deleteCar), 1);
+    }
+
+    publishCars();
+}
+
+function allCarsList() {
+    publishCars();
+}
+
+function dyzelCarsList() {
+    publishCars(FuelType.Dyzelis);
+}
+
+function gasCarsList() {
+    publishCars(FuelType.Benzinas);
+}
